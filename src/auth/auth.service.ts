ec6d0a6e575a52fb11d31e41071a806ce58ab14b
Thiago Salgado <thiagobtw437@gmail.com>
2024-08-27 15:07:40 +1000

    Finished implementation of contact message endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import * as bcrpyt from 'bcrypt';
+
+interface Payload {
+  access_token: string;
+}
 
 @Injectable()
 export class AuthService {
@@ -10,7 +18,10 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async signIn(username: string, pass: string): Promise<Payload> {
+    if (!username || !pass) {
+      throw new BadRequestException('Username or password not provided.');
+    }
     const user = await this.usersService.findOne(username);
 
     if (!bcrpyt.compare(pass, user.password)) {
--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,13 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterNewMessageDTO } from './dto/register-new-message.dto';
-import { Public } from 'src/config/metadata';
 import { MessageService } from './message.service';
 
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
-  @Public()
+  // @Public()
   @Post()
   async registerMessage(@Body() registerMessageDto: RegisterNewMessageDTO) {
     return this.messageService.register(registerMessageDto);
