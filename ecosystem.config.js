58cea45c35885c746b52c8add099b6df51676d3d
Thiago Salgado <thiagobtw437@gmail.com>
2025-05-07 16:03:03 +1000

    Added necessary depedency before declared as a dev dependency, it is necessary for build the package (nest/cli).

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    apps: [
-        {
-            name: 'my-page-api',
-            script: 'dist/main.js',
-            env: {
-                NODE_ENV: 'production',
-                DATABASE_URL: process.env.DATABASE_URL
-            }
-        },
-        {
-            name: 'metrics-server',
-            script: './metrics.js'
-        }
-    ]
-}
+  apps: [
+    {
+      name: 'my-page-api',
+      script: 'dist/main.js',
+      env: {
+        NODE_ENV: 'production',
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+    },
+    {
+      name: 'metrics-server',
+      script: './metrics.js',
+    },
+  ],
+};
