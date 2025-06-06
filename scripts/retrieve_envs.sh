7accc73dee596ecd95452f55674574d0100d8807
Thiago Salgado <thiagobtw437@gmail.com>
2025-06-06 19:13:36 +1000

    Changed dir of creation for .env file, should create on the root dir now.

---
+++ scripts/retrieve_envs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script is run before the installation of the package.
+# It is used to retrieve the environment variables from AWS Systems Manager Parameter Store.
+# and writes them to a .env file in the current directory.
+
+# Create and set permissions for the .env file
+sudo touch ".env" # Create the .env file if it doesn't exist
+sudo chmod 600 ".env" # Set permissions to read/write for the owner only
+sudo chown $USER:$USER ".env" # Change ownership to the current user
+
+# Retrieve secrets from AWS SSM Parameter Store
+# and write them to the .env file
+aws ssm get-parameters-by-path \
+  --path "/mypageapi/env" \
+  --with-decryption \
+  --query "Parameters[*].{Name:Name,Value:Value}" \
+  --region ap-southeast-2 \
+  --output text | 
+  while read name value; do
+    # Extract the key without the path prefix
+    key=$(basename "$name")
+    # Extract the value into output file
+    echo "$key=\"$value\"" >> ".env"
+  done
